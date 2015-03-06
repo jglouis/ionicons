5fcc91c0fad3621e28ca0b1577ff116cabf0f8b8
jgl <jgl@spaceapplications.com>
2015-03-06 13:08:11 +0100

    Recolour scripts writes now in a new file and delete the old one.

--- src/recolour.sh
+++ src/recolour.sh
@@ -5,7 +5,8 @@
 for f in *.svg
 do
 
-sed "s/fill:#111111/fill:none;/g" < $f | sed "s/fill:#111;/fill:none;/g" | sed "s/stroke:#eeeeee/stroke:none;/g" | sed "s/stroke:#eee;/stroke:none;/g" | sed "s/fill:white/fill:#000000;/g" | sed "s/stroke:white/stroke:#000000;/g" | sed "s/fill:#ffffff/fill:#000000;/g" | sed "s/stroke:#ffffff/stroke:#000000;/g" > $f
+sed "s/fill:#111111/fill:none;/g" < $f | sed "s/fill:#111;/fill:none;/g" | sed "s/stroke:#eeeeee/stroke:none;/g" | sed "s/stroke:#eee;/stroke:none;/g" | sed "s/fill:white/fill:#000000;/g" | sed "s/stroke:white/stroke:#000000;/g" | sed "s/fill:#ffffff/fill:#000000;/g" | sed "s/stroke:#ffffff/stroke:#000000;/g" > "android-$f"
 
+rm $f
 
 done
