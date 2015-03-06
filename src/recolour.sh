ac4f8ed8f950af3aeda0cbd587bb92cc1cf233c9
jgl <jgl@spaceapplications.com>
2015-03-06 12:50:20 +0100

    Modified recolour script so that it writes the output into a given file.

--- src/recolour.sh
+++ src/recolour.sh
@@ -4,5 +4,7 @@
 # $2 background fill
 # $3 background stroke
 # $4 forground
+# $5 new file
 
-sed "s/fill:#111111/fill:${2};/g" < $1 | sed "s/fill:#111;/fill:${2};/g" | sed "s/stroke:#eeeeee/stroke:${3};/g" | sed "s/stroke:#eee;/stroke:${3};/g" | sed "s/fill:white/fill:${4};/g" | sed "s/stroke:white/stroke:${4};/g" | sed "s/fill:#ffffff/fill:${4};/g" | sed "s/stroke:#ffffff/stroke:${4};/g"
+
+sed "s/fill:#111111/fill:${2};/g" < $1 | sed "s/fill:#111;/fill:${2};/g" | sed "s/stroke:#eeeeee/stroke:${3};/g" | sed "s/stroke:#eee;/stroke:${3};/g" | sed "s/fill:white/fill:${4};/g" | sed "s/stroke:white/stroke:${4};/g" | sed "s/fill:#ffffff/fill:${4};/g" | sed "s/stroke:#ffffff/stroke:${4};/g" > "${5}"
