398b079ca7f9c4cba1c9f9e75d6ff00d466c7e1b
jgl <jgl@spaceapplications.com>
2015-03-09 10:32:57 +0100

    Changed _ to -. Updated the recolour script to automate that step.

--- src/recolour.sh
+++ src/recolour.sh
@@ -5,8 +5,12 @@
 for f in *.svg
 do
 
-sed "s/fill:#111111/fill:none;/g" < $f | sed "s/fill:#111;/fill:none;/g" | sed "s/fill:white/fill:#000000;/g" | sed "s/stroke:white/stroke:#000000;/g" | sed "s/fill:#ffffff/fill:#000000;/g" | sed "s/stroke:#ffffff/stroke:#000000;/g" > "$f"
+sed "s/fill:#111111/fill:none;/g" < $f | sed "s/fill:#111;/fill:none;/g" | sed "s/fill:white/fill:#000000;/g" | sed "s/stroke:white/stroke:#000000;/g" | sed "s/fill:#ffffff/fill:#000000;/g" | sed "s/stroke:#ffffff/stroke:#000000;/g" > "new_$f"
 
 rm $f
+
+rename 'y/_/-/' *.svg
+mv "$file" "${file:4}"
+
 
 done
