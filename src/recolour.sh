888f4d4788eff79a7985b1057801d9558795b9d5
jgl <jgl@spaceapplications.com>
2015-03-09 10:11:00 +0100

    Recolour script does not add the android prefix anymore.

--- src/recolour.sh
+++ src/recolour.sh
@@ -5,7 +5,7 @@
 for f in *.svg
 do
 
-sed "s/fill:#111111/fill:none;/g" < $f | sed "s/fill:#111;/fill:none;/g" | sed "s/fill:white/fill:#000000;/g" | sed "s/stroke:white/stroke:#000000;/g" | sed "s/fill:#ffffff/fill:#000000;/g" | sed "s/stroke:#ffffff/stroke:#000000;/g" > "android-$f"
+sed "s/fill:#111111/fill:none;/g" < $f | sed "s/fill:#111;/fill:none;/g" | sed "s/fill:white/fill:#000000;/g" | sed "s/stroke:white/stroke:#000000;/g" | sed "s/fill:#ffffff/fill:#000000;/g" | sed "s/stroke:#ffffff/stroke:#000000;/g" > "$f"
 
 rm $f
 
